57deecada9f5f1f76c4e0e1b18541ffa2bd9f18c
Gnanendra JN <gnanendra.jn@gmail.com>
2023-06-23 13:34:27 +0530

    First commit

---
+++ src/main/java/org/learn/java/nookncorner/JavaNookNCornerApplication.java
@@ -0,0 +1,13 @@
+package org.learn.java.nookncorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNookNCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNookNCornerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/learn/java/nookncorner/JavaNookNCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.java.nookncorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNookNCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
